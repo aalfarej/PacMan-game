bd6e5b5b9d4d81e819731ac5ed68d3edbf8653b5
Ali Alfarej <aalfarej2019@kewforest.org>
2018-03-06 09:56:56 -0500

    first attempt

--- MAIN.py
+++ MAIN.py
@@ -1,8 +1,18 @@
+import random, os.path
+
+#import basic pygame modules
 import pygame
+from pygame.locals import *
+
+#see if we can load more than standard BMP
+if not pygame.image.get_extended():
+    raise SystemExit("Sorry, extended image module required")
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 def load_image(file):
     "loads an image, prepares it for play"
-    file = os.path.join(main_dir, 'data', file)
+    file = os.path.join(main_dir, 'Data', file)
     try:
         surface = pygame.image.load(pac.svg)
     except pygame.error:
